af7ea8ffeb6f3f222c06ea478a1922ae09202ab6
pedrolandaacurio <pedrolandaacurio@hotmail.com>
2017-11-07 16:40:51 -0500

    Agregar archivos de proyecto.

---
+++ FormularioJM_Prueba1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FormularioJM_Prueba1.MvcApplication" Language="C#" %>
---
+++ FormularioJM_Prueba1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FormularioJM_Prueba1.Startup))]
+namespace FormularioJM_Prueba1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
